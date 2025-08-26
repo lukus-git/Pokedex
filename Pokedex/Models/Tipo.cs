5ef9265f305ccd103a592ff1e265e9392dc59a12
lukus-git <lucas.garbi13@hotmail.com>
2025-08-25 22:36:37 -0300

    Criando um projeto e a camada de dados

---
+++ Pokedex/Models/Tipo.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Pokedex.Models;
+[Table("Tipo")]
+public class Tipo
+{
+    [Key]
+    public uint Id { get; set; }
+    
+    [StringLength(30)]
+    [Required(ErrorMessage ="Por favor, informe o nome")]
+
+    public string Nome { get; set; }
+
+    [StringLength(25)]
+    public string Cor { get; set; }
+}
